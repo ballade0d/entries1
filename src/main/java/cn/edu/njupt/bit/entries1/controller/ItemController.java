ab79463a8867cd60390b2f6b09c158e528c7f3e9
ballade0d <ballade0d@gmail.com>
2023-10-24 12:00:41 +0800

    feat. icons, pagination, modal

--- src/main/java/cn/edu/njupt/bit/entries1/controller/ItemController.java
+++ src/main/java/cn/edu/njupt/bit/entries1/controller/ItemController.java
@@ -7,31 +7,28 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:63342")
 @RestController
 public class ItemController {
 
     @Resource
     private ItemMapper itemMapper;
 
-    @CrossOrigin(origins = "http://localhost:63343")
     @GetMapping("/listItem")
     public List<Map<String, Object>> listItem() {
         return itemMapper.listItem();
     }
 
-    @CrossOrigin(origins = "http://localhost:63343")
     @GetMapping("/listShoppingCart")
     public List<Map<String, Object>> listShoppingCart() {
         return itemMapper.listShoppingCart();
     }
 
-    @CrossOrigin(origins = "http://localhost:63343")
     @PostMapping("/addToCart")
     public void addToCart(@RequestParam int id) {
         itemMapper.addToCart(id);
     }
 
-    @CrossOrigin(origins = "http://localhost:63343")
     @PostMapping("/removeFromCart")
     public void removeFromCart(@RequestParam int id) {
         itemMapper.removeFromCart(id);
