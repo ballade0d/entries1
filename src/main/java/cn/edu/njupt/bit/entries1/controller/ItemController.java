24c8c742870b87e0d7892c8645cd64451f2617f7
ballade0d <ballade0d@gmail.com>
2023-10-26 09:22:50 +0800

    optimize code

--- src/main/java/cn/edu/njupt/bit/entries1/controller/ItemController.java
+++ src/main/java/cn/edu/njupt/bit/entries1/controller/ItemController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:63343")
+@CrossOrigin(origins = {"http://localhost:63343", "http://localhost:63342"})
 @RestController
 public class ItemController {
 
